4fec4734515ff71173688e42788aa34cab5f97cd
DominikBogielczyk <59863153+DominikBogielczyk@users.noreply.github.com>
2023-07-31 18:25:10 +0200

    Arduino code refactor

--- 8x8_and_HC_06/8x8_and_HC_06.ino
+++ 8x8_and_HC_06/8x8_and_HC_06.ino
@@ -4,11 +4,6 @@
 #define CS 9
 #define CLK 8
 
-#define left_switch 11
-#define right_switch 13
-#define up_switch 12
-#define down_switch 7
-
 LedControl lc = LedControl(DIN, CLK, CS, 4);  //LedControl(dataPin,clockPin,csPin,numDevices)
 
 String modes = "    ";  //'h' - heart, 'p' - pacman, 's' - stairs, 't' - text
@@ -17,48 +12,50 @@ String modes = "    ";  //'h' - heart, 'p' - pacman, 's' - stairs, 't' - text
 byte heart_pattern[7] = {0x66, 0x99, 0x81, 0x81, 0x42, 0x24, 0x18};
 byte pacman1[7] = {0x38, 0x44, 0x88, 0x90, 0x88, 0x44, 0x38};
 byte pacman2[7] = {0x38, 0x44, 0x82, 0x82, 0x82, 0x44, 0x38};
-byte zero[5] = {0x07, 0x05, 0x05, 0x05, 0x07};
-byte one[5] = {0x01, 0x01, 0x01, 0x01, 0x01};
-byte two[5] = {0x07, 0x01, 0x07, 0x04, 0x07};
-byte three[5] = {0x07, 0x01, 0x07, 0x01, 0x07};
-byte four[5] = {0x05, 0x05, 0x07, 0x01, 0x01};
-byte five[5] = {0x07, 0x04, 0x07, 0x01, 0x07};
-byte six[5] = {0x07, 0x04, 0x07, 0x05, 0x07};
-byte seven[5] = {0x07, 0x01, 0x01, 0x01, 0x01};
-byte eight[5] = {0x07, 0x05, 0x07, 0x05, 0x07};
-byte nine[5] = {0x07, 0x05, 0x07, 0x01, 0x07};
-byte *numbers[10] = {zero, one, two, three, four, five, six, seven, eight, nine};
+byte numbers[10][5] = {
+  {0x07, 0x05, 0x05, 0x05, 0x07}, // 0
+  {0x01, 0x01, 0x01, 0x01, 0x01}, // 1
+  {0x07, 0x01, 0x07, 0x04, 0x07}, // 2
+  {0x07, 0x01, 0x07, 0x01, 0x07}, // 3
+  {0x05, 0x05, 0x07, 0x01, 0x01}, // 4
+  {0x07, 0x04, 0x07, 0x01, 0x07}, // 5
+  {0x07, 0x04, 0x07, 0x05, 0x07}, // 6
+  {0x07, 0x01, 0x01, 0x01, 0x01}, // 7
+  {0x07, 0x05, 0x07, 0x05, 0x07}, // 8
+  {0x07, 0x05, 0x07, 0x01, 0x07}  // 9
+};
 byte dice5[3] = {0xA0, 0x40, 0xA0};
 byte dice6[3] = {0xA0, 0xA0, 0xA0};
 
 //letters
-byte A[7] = {0x38, 0x44, 0x44, 0x44, 0x7C, 0x44, 0x44};
-byte B[7] = {0x3C, 0x22, 0x22, 0x3C, 0x22, 0x22, 0x3C};
-byte C[7] = {0x1C, 0x20, 0x20, 0x20, 0x20, 0x20, 0x1C};
-byte D[7] = {0x38, 0x24, 0x22, 0x22, 0x22, 0x24, 0x38};
-byte E[7] = {0x3E, 0x20, 0x20, 0x3C, 0x20, 0x20, 0x3E};
-byte F[7] = {0x3E, 0x20, 0x20, 0x3C, 0x20, 0x20, 0x20};
-byte G[7] = {0x1C, 0x22, 0x20, 0x2E, 0x22, 0x22, 0x1E};
-byte H[7] = {0x22, 0x22, 0x22, 0x3E, 0x22, 0x22, 0x22};
-byte I[7] = {0x1C, 0x08, 0x08, 0x08, 0x08, 0x08, 0x1C};
-byte J[7] = {0x38, 0x08, 0x08, 0x08, 0x08, 0x28, 0x18};
-byte K[7] = {0x22, 0x24, 0x28, 0x30, 0x28, 0x24, 0x22};
-byte L[7] = {0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x3E};
-byte M[7] = {0x22, 0x36, 0x2A, 0x2A, 0x22, 0x22, 0x22};
-byte N[7] = {0x22, 0x22, 0x32, 0x2A, 0x26, 0x22, 0x22};
-byte O[7] = {0x1C, 0x22, 0x22, 0x22, 0x22, 0x22, 0x1C};
-byte P[7] = {0x3C, 0x22, 0x22, 0x3C, 0x20, 0x20, 0x20};
-byte Q[7] = {0x1C, 0x22, 0x22, 0x22, 0x2A, 0x24, 0x1A};
-byte R[7] = {0x3C, 0x22, 0x22, 0x3C, 0x28, 0x24, 0x22};
-byte S[7] = {0x1E, 0x20, 0x20, 0x1C, 0x02, 0x02, 0x3C};
-byte T[7] = {0x3E, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08};
-byte U[7] = {0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x1C};
-byte V[7] = {0x22, 0x22, 0x22, 0x22, 0x22, 0x14, 0x08};
-byte W[7] = {0x22, 0x22, 0x22, 0x2A, 0x2A, 0x2A, 0x14};
-byte X[7] = {0x22, 0x22, 0x14, 0x08, 0x14, 0x22, 0x22};
-byte Y[7] = {0x22, 0x22, 0x22, 0x14, 0x08, 0x08, 0x08};
-byte Z[7] = {0x3E, 0x02, 0x04, 0x08, 0x10, 0x20, 0x3E};
-byte *letters[26] = {A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z}; //ASCII TABLE: 65 - 'A', 66 - 'B', ...
+byte letters[26][7] = {                       // ASCII TABLE: 65 - 'A', 66 - 'B', ...
+  {0x38, 0x44, 0x44, 0x44, 0x7C, 0x44, 0x44}, // A
+  {0x3C, 0x22, 0x22, 0x3C, 0x22, 0x22, 0x3C}, // B
+  {0x1C, 0x20, 0x20, 0x20, 0x20, 0x20, 0x1C}, // C
+  {0x38, 0x24, 0x22, 0x22, 0x22, 0x24, 0x38}, // D
+  {0x3E, 0x20, 0x20, 0x3C, 0x20, 0x20, 0x3E}, // E
+  {0x3E, 0x20, 0x20, 0x3C, 0x20, 0x20, 0x20}, // F
+  {0x1C, 0x22, 0x20, 0x2E, 0x22, 0x22, 0x1E}, // G
+  {0x22, 0x22, 0x22, 0x3E, 0x22, 0x22, 0x22}, // H
+  {0x1C, 0x08, 0x08, 0x08, 0x08, 0x08, 0x1C}, // I
+  {0x38, 0x08, 0x08, 0x08, 0x08, 0x28, 0x18}, // J
+  {0x22, 0x24, 0x28, 0x30, 0x28, 0x24, 0x22}, // K
+  {0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x3E}, // L
+  {0x22, 0x36, 0x2A, 0x2A, 0x22, 0x22, 0x22}, // M
+  {0x22, 0x22, 0x32, 0x2A, 0x26, 0x22, 0x22}, // N
+  {0x1C, 0x22, 0x22, 0x22, 0x22, 0x22, 0x1C}, // O
+  {0x3C, 0x22, 0x22, 0x3C, 0x20, 0x20, 0x20}, // P
+  {0x1C, 0x22, 0x22, 0x22, 0x2A, 0x24, 0x1A}, // Q
+  {0x3C, 0x22, 0x22, 0x3C, 0x28, 0x24, 0x22}, // R
+  {0x1E, 0x20, 0x20, 0x1C, 0x02, 0x02, 0x3C}, // S
+  {0x3E, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08}, // T
+  {0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x1C}, // U
+  {0x22, 0x22, 0x22, 0x22, 0x22, 0x14, 0x08}, // V
+  {0x22, 0x22, 0x22, 0x2A, 0x2A, 0x2A, 0x14}, // W
+  {0x22, 0x22, 0x14, 0x08, 0x14, 0x22, 0x22}, // X
+  {0x22, 0x22, 0x22, 0x14, 0x08, 0x08, 0x08}, // Y
+  {0x3E, 0x02, 0x04, 0x08, 0x10, 0x20, 0x3E}  // Z
+};
 
 byte zero_big[7] = {0x1C, 0x22, 0x22, 0x22, 0x22, 0x22, 0x1C};
 byte one_big[7] = {0x08, 0x18, 0x08, 0x08, 0x08, 0x08, 0x1C};
@@ -95,74 +92,27 @@ void setup()
   }
 
   randomSeed(analogRead(0)); //random generator initialization, noise from analog input
-  
-  pinMode(left_switch, INPUT_PULLUP);
-  pinMode(right_switch, INPUT_PULLUP);
-  pinMode(up_switch, INPUT_PULLUP);
-  pinMode(down_switch, INPUT_PULLUP);
-
   Serial.begin(9600);
 }
 
 void loop()
 {
   if (Serial.available())
-  {
     receive_data();
-  }
-  
+
   //GIFs
   for (int i = 0; i < modes.length(); i++)
   {
-    if (modes.indexOf('s') != -1) //stairs
-    {
-      if (millis() - stairs_t >= 4000 / prescaler)
-      {
-        stairs();
-        stairs_i++;
-        if (stairs_i == 8) stairs_i = 0;
-        stairs_t = millis();
-      }
-    }
-    if ((modes.indexOf('p') != -1))  //pacman
-    {
-      if (millis() - pacman_t >= 2000 / prescaler)
-      {
-        pacman();
-        pacman_i++;
-        if (pacman_i == 5) pacman_i = 0;
-        pacman_t = millis();
-      }
-    }
-    if (modes.indexOf('h') != -1) //heart
-    {
-      if (millis() - heart_t >= 2000 / prescaler)
-      {
-        heart_t = millis();
-        heart(heart_state);
-      }
-    }
-    if (modes.indexOf('r') != -1)
-    {
-      if (millis() - random_num_t >= 4000 / prescaler)
-      {
-        random_num_t = millis();
-        random_pattern();
-      }
-    }
-    if (modes.indexOf('t') != -1)
-    {
-      if (millis() - text_t >= 3000 / prescaler)
-      {
-        text_t = millis();
-        if (text_i > entered_text.length() + 2)
-        {
-          text_i = 0;
-        }
-        display_text(entered_text, text_i);
-        text_i++;
-      }
-    }
+    if (modes.indexOf('s') != -1 and millis() - stairs_t >= 4000 / prescaler) //stairs
+      stairs();
+    if (modes.indexOf('p') != -1 and millis() - pacman_t >= 2000 / prescaler)
+      pacman();
+    if (modes.indexOf('h') != -1 and millis() - heart_t >= 2000 / prescaler)
+      heart(heart_state);
+    if (modes.indexOf('r') != -1 and millis() - random_num_t >= 4000 / prescaler)
+      random_pattern();
+    if (modes.indexOf('t') != -1 and millis() - text_t >= 3000 / prescaler)
+      display_text(entered_text, text_i);
   }
 }
 
@@ -174,38 +124,25 @@ void receive_data()
   for (int i = 0; i < 4; i++)
   {
     if (received_data.indexOf(cmd[i]) != -1)  //TOGGLE SCREEN ON/OFF STATE
-    {
       matrixes[i] = !matrixes[i];
-    }
   }
 
-  if (received_data.indexOf("sl") != -1) //SLIDER HANDLE
-  {
-    if (prescaler >= 1 and prescaler <= 9)
-    {
-      prescaler = received_data[received_data.indexOf("sl") + 2] - 48; //from ACII table '0' = 48, '1' = 49, ...
-    }
-  }
+  if (received_data.indexOf("sl") != -1 and prescaler >= 1 and prescaler <= 9) //SLIDER HANDLE
+    prescaler = received_data[received_data.indexOf("sl") + 2] - 48; //from ACII table '0' = 48, '1' = 49, ...
+
   if (received_data.indexOf("dice") != -1)
-  {
     dice();
-  }
   else if (received_data.indexOf("snake") != -1)
-  {
     snake();
-  }
   else if (received_data.indexOf("draw") != -1)
-  {
-    draw();
-  }
+    random_numbers();
   else if (received_data.indexOf("*") != -1)
   {
     entered_text = received_data.substring(received_data.indexOf("*") + 1);
     entered_text.toUpperCase();
     text_i = 0;
   }
-  else if (received_data.indexOf("heart") != -1 or received_data.indexOf("pacman") != -1 or received_data.indexOf("stairs") != -1 or received_data.indexOf("random") != -1
-           or received_data.indexOf("text") != -1)
+  else if (received_data.length() > 0)
   {
     String tab[5] = {"heart", "pacman", "stairs", "random", "text"};
 
@@ -214,27 +151,20 @@ void receive_data()
       if (received_data.indexOf(tab[j]) != -1)
       {
         if (received_data == "text")
-        {
           modes = "tttt";
-        }
         else
         {
           for (int i = 0; i < 4; i++)
           {
             if (matrixes[i])
-            {
               modes[i] = tab[j][0];
-            }
           }
         }
-
       }
     }
   }
   else if (received_data.indexOf("clear") != -1)
-  {
     clear();
-  }
 }
 
 void print_pattern(byte *value, uint8_t n, uint8_t addr)
@@ -252,11 +182,10 @@ void heart(bool state)
   for (int i = 0; i < 4; i++)
   {
     if (modes[i] == 'h')
-    {
       state == 1 ? print_pattern(heart_pattern, sizeof(heart_pattern), i) : lc.clearDisplay(i);
-    }
   }
   heart_state = !state; //TOGGLE HEART STATE
+  heart_t = millis();
 }
 
 void pacman()
@@ -265,10 +194,7 @@ void pacman()
   {
     for (int i = 0; i < 4; i++)
     {
-      if (modes[i] == 'p')
-      {
-        print_pattern(pacman1, sizeof(pacman1), i); //FIRST FRAME}
-      }
+      if (modes[i] == 'p') print_pattern(pacman1, sizeof(pacman1), i); //FIRST FRAME}
     }
   }
   else if (pacman_i >= 0 and pacman_i < 4)
@@ -282,16 +208,19 @@ void pacman()
       }
     }
   }
-  else
+  else // pacman_i == 4 - END FRAME
   {
     for (int i = 0; i < 4; i++)
     {
-      if (modes[i] == 'p') print_pattern(pacman2, sizeof(pacman2), i); //END FRAME
+      if (modes[i] == 'p') print_pattern(pacman2, sizeof(pacman2), i);
     }
   }
+  pacman_i++;
+  if (pacman_i == 5) pacman_i = 0;
+  pacman_t = millis();
 }
 
-void draw()
+void random_numbers()
 {
   reset_mode();
   for (int i = 0; i < 10; i++) //10 RANDOM NUMBERS
@@ -300,13 +229,12 @@ void draw()
     {
       if (matrixes[j])
       {
-        uint8_t v = random(100); //<0;99>
-        uint8_t v1 = v / 10;
-        uint8_t v2 = v % 10;
-        byte x[7] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+        uint8_t v1 = random(10); // Generates a random number between 0 and 9
+        uint8_t v2 = random(10);
+        byte x[7] = {};
         for (int i = 0; i < 5; i++)
         {
-          x[i + 2] = (numbers[v1][i] << 4) + numbers[v2][i]; 
+          x[i + 2] = (numbers[v1][i] << 4) + numbers[v2][i];
         }
         print_pattern(x, sizeof(x), j);
       }
@@ -318,14 +246,14 @@ void draw()
 void dice()
 {
   reset_mode();
-  for (int j = 0; j < 7; j++) //7 DICES DRAWING
+  for (int j = 0; j < 7; j++) //7 DICES ANIMATIONS
   {
     for (int k = 0; k < 4; k++)
     {
       if (matrixes[k])
       {
         lc.clearDisplay(k);
-        uint8_t v[2] = {(uint8_t) random(1, 7), (uint8_t) random(1, 7)}; // 2x <1; 6>
+        uint8_t v[2] = {random(1, 7), random(1, 7)}; // 2x <1; 6>
 
         for (int i = 0; i < 2; i++)
         {
@@ -373,10 +301,7 @@ void stairs()
   {
     for (int i = 0; i < 4; i++)
     {
-      if (modes[i] == 's')
-      {
-        lc.clearDisplay(i);
-      }
+      if (modes[i] == 's') lc.clearDisplay(i);
     }
   }
 
@@ -384,12 +309,12 @@ void stairs()
   {
     for (int k = 0; k < 4; k++)
     {
-      if (modes[k] == 's')
-      {
-        lc.setLed(k, j, stairs_i, 1);
-      }
+      if (modes[k] == 's') lc.setLed(k, j, stairs_i, 1);
     }
   }
+  stairs_i++;
+  if (stairs_i == 8) stairs_i = 0;
+  stairs_t = millis();
 }
 
 int snake()
@@ -439,23 +364,19 @@ int snake()
           fruit[1] = random(8);
           bool unique_x = 1;
           bool unique_y = 1;
-          
-          for(int i=0; i<snake_size; i++)
+
+          for (int i = 0; i < snake_size; i++)
           {
-            if(pos_x[i] == fruit[0])
-            {
+            if (pos_x[i] == fruit[0])
               unique_x = 0;
-            }
-            if(pos_y[i] == fruit[1])
-            {
+            if (pos_y[i] == fruit[1])
               unique_y = 0;
-            }
           }
           unique = unique_x and unique_y;
         }
 
-        snake_size += 1;
-        
+        snake_size ++;
+
         //FRUIT DRAW
         lc.setLed(3 - fruit[0] / 8, fruit[1], fruit[0] % 8, 1);
 
@@ -476,21 +397,13 @@ int snake()
       }
 
       if (dir == "left")
-      {
         pos_x[0] - 1 >= 0 ? pos_x[0] -= 1 : pos_x[0] = 23;
-      }
       else if (dir == "right")
-      {
         pos_x[0] + 1 <= 23 ? pos_x[0] += 1 : pos_x[0] = 0;
-      }
       else if (dir == "up")
-      {
         pos_y[0] - 1 >= 0 ? pos_y[0] -= 1 : pos_y[0] = 7;
-      }
       else if (dir == "down")
-      {
         pos_y[0] + 1 <= 7 ? pos_y[0] += 1 : pos_y[0] = 0;
-      }
 
       //UPDATE SNAKE HEAD
       lc.setLed(3 - pos_x[0] / 8, pos_y[0], pos_x[0] % 8, 1);
@@ -504,43 +417,29 @@ int snake()
         for (int j = 0; j < i; j++)
         {
           if (pos_x[i] == pos_x[j] and pos_y[i] == pos_y[j])
-          {
             end_game = true;
-          }
         }
       }
     }
 
     uint8_t dir_cmd = 0;
     if (Serial.available())
-    {
       dir_cmd = Serial.read();
-    }
 
-    if ((digitalRead(right_switch) == 0 or dir_cmd == 114) and dir != "left")
-    {
+    if (dir_cmd == 114 and dir != "left")
       dir = "right";
-    }
-    else if ((digitalRead(left_switch) == 0 or dir_cmd == 108) and dir != "right")
-    {
+    else if (dir_cmd == 108 and dir != "right")
       dir = "left";
-    }
-    else if ((digitalRead(up_switch) == 0 or dir_cmd == 117) and dir != "down")
-    {
+    else if (dir_cmd == 117 and dir != "down")
       dir = "up";
-    }
-    else if ((digitalRead(down_switch) == 0 or dir_cmd == 100) and dir != "up")
-    {
+    else if (dir_cmd == 100 and dir != "up")
       dir = "down";
-    }
     else if (dir_cmd == 113) //'q'
-    {
       return 0;
-    }
   }
   //WAIT 5SEC
   delay(5000);
-  
+
   //CLEAR POINTS DISPLAY
   lc.clearDisplay(0);
 
@@ -553,9 +452,7 @@ void reset_mode()
   for (int i = 0; i < 4; i++)
   {
     if (matrixes[i])
-    {
       modes[i] = ' ';
-    }
   }
 }
 
@@ -576,14 +473,17 @@ void random_pattern()
   for (int i = 0; i < 4; i++)
   {
     if (modes[i] == 'r')
-    {
       print_pattern(rand_num, sizeof(rand_num), i);
-    }
   }
+  random_num_t = millis();
 }
 
 void display_text(String text, uint8_t index)
 {
+  text_t = millis();
+  if (text_i > entered_text.length() + 2)
+    text_i = 0;
+
   for (int i = 0; i < 4; i++)
   {
     lc.clearDisplay(i);
@@ -594,13 +494,10 @@ void display_text(String text, uint8_t index)
     if (index - i >= 0)
     {
       if ((int)text[index - i] >= 65 and (int)text[index - i] <= 90) //LETTERS
-      {
         print_pattern(letters[(int)text[index - i] - 65], 7, i);
-      }
       else if ((int)text[index - i] >= 48 and (int)text[index - i] <= 57) //NUMBERS
-      {
         print_pattern(numbers_big[(int)text[index - i] - 48], 7, i);
-      }
     }
   }
+  text_i++;
 }
